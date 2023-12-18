06afb4711ca7e361ff118f1b3365dc3a3dd388ac
ogius <kowalewski.olgierd@gmail.com>
2023-12-18 18:16:36 +0100

    Repaired cost function

--- src/network/utils/cost.cpp
+++ src/network/utils/cost.cpp
@@ -1,6 +1,5 @@
-double countCost(double *output, double *expected)
+double countCost(double *output, double *expected, int columnsAmount)
 {
-  int columnsAmount = sizeof(output) / sizeof(output[0]);
   double cost = 0.0;
   for (int i = 0; i < columnsAmount; i++)
     cost += (expected[i] - output[i]) * (expected[i] - output[i]);
