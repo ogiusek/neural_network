8fe8f23f334aa8aa1f5d2495b29d895a22922b6a
ogius <kowalewski.olgierd@gmail.com>
2024-01-20 16:52:39 +0100

    Added to measure time start and stop

--- src/utils/measureTime.cpp
+++ src/utils/measureTime.cpp
@@ -1,14 +1,26 @@
 #include <functional>
 #include <chrono>
+#include <map>
+#include <string>
 
 namespace Utils
 {
-  // returns time in nanoseconds
-  int measureTime(std::function<void()> func)
+  namespace Time
   {
-    auto start = std::chrono::high_resolution_clock::now();
-    func();
-    auto end = std::chrono::high_resolution_clock::now();
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
-  }
+    // returns time in nanoseconds
+    int measureTime(std::function<void()> func)
+    {
+      auto start = std::chrono::high_resolution_clock::now();
+      func();
+      auto end = std::chrono::high_resolution_clock::now();
+      return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+    }
+
+    std::map<std::string, std::chrono::high_resolution_clock::time_point> timeMeasurements;
+
+    void start(std::string name) { timeMeasurements[name] = std::chrono::high_resolution_clock::now(); }
+
+    // returns time in nanoseconds
+    size_t stop(std::string name) { return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::high_resolution_clock::now() - timeMeasurements[name]).count(); }
+  };
 };
